6eafcbc6786d545c3f291eef78a8f1ec8d6842f9
habrit <hbritton@tamu.edu>
2023-09-29 20:03:57 -0500

    Labs

---
+++ ecen454_714/4bitAdder_run1/INCA_libs/irun.lnx8664.15.20.nc/files.ts
@@ -0,0 +1,5 @@
+1675829983 /home/ugrads/h/hbritton/ecen454_714/4bitAdder_run1/testfixture.template
+1139589197 /opt/coe/ncsu/ncsu-cdk-1.6.0.beta/lib/NCSU_Digital_Parts/nand2/functional/verilog.v
+1139589200 /opt/coe/ncsu/ncsu-cdk-1.6.0.beta/lib/NCSU_Digital_Parts/xor2/functional/verilog.v
+1675829983 /home/ugrads/h/hbritton/ecen454_714/4bitAdder_run1/ihnl/cds0/netlist
+1675829983 /home/ugrads/h/hbritton/ecen454_714/4bitAdder_run1/ihnl/cds1/netlist
---
+++ ecen454_714/8bitAdder_run1/INCA_libs/snap.nc/.timestamp.ts
@@ -0,0 +1,10 @@
+1675882070 /home/ugrads/h/hbritton/ecen454_714/8bitAdder_run1/testfixture.template
+1675882070 /home/ugrads/h/hbritton/ecen454_714/8bitAdder_run1/ihnl/cds4/netlist
+1139589194 /opt/coe/ncsu/ncsu-cdk-1.6.0.beta/lib/NCSU_Digital_Parts/Dlatch/behavioral/verilog.v
+1139589200 /opt/coe/ncsu/ncsu-cdk-1.6.0.beta/lib/NCSU_Digital_Parts/xor2/functional/verilog.v
+1675882070 /home/ugrads/h/hbritton/ecen454_714/8bitAdder_run1/ihnl/cds2/netlist
+1675882070 /home/ugrads/h/hbritton/ecen454_714/8bitAdder_run1/ihnl/cds0/netlist
+1139589195 /opt/coe/ncsu/ncsu-cdk-1.6.0.beta/lib/NCSU_Digital_Parts/inv/functional/verilog.v
+1675882070 /home/ugrads/h/hbritton/ecen454_714/8bitAdder_run1/ihnl/cds3/netlist
+1675882070 /home/ugrads/h/hbritton/ecen454_714/8bitAdder_run1/ihnl/cds1/netlist
+1139589197 /opt/coe/ncsu/ncsu-cdk-1.6.0.beta/lib/NCSU_Digital_Parts/nand2/functional/verilog.v
